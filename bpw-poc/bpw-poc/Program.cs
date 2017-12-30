dec82c71c5ae7f0955bb88eabe2db809826c0ba8
manish <manish10@gmail.com>
2017-12-30 01:11:55 -0500

    the .net poc.

---
+++ bpw-poc/bpw-poc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Google.Cloud.Vision.V1;
+namespace bpw_poc
+{
+   public  class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = ImageAnnotatorClient.Create();
+            var image = Image.FromUri("gs://bpw-poc-images/property-tax.jpg");
+            var     response = client.DetectDocumentText(image);
+            Console.WriteLine(response.Text);
+            Console.ReadLine();
+        }
+    }
+}
