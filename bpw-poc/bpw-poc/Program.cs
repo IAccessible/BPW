c01cf86f1af8c843f7ec563f9b059342d5a9346f
manish <manish10@gmail.com>
2018-01-15 12:06:51 -0500

    added pages, blocks and para.

--- bpw-poc/bpw-poc/Program.cs
+++ bpw-poc/bpw-poc/Program.cs
@@ -2,13 +2,36 @@
 using Google.Cloud.Vision.V1;
 namespace bpw_poc
 {
-   public  class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             var client = ImageAnnotatorClient.Create();
             var image = Image.FromUri("gs://bpw-poc-images/property-tax.jpg");
-            var     response = client.DetectDocumentText(image);
+            var response = client.DetectDocumentText(image);
+            foreach (var page in response.Pages)
+            {
+                foreach (var block in page.Blocks)
+                {
+                    if (block.BlockType == Block.Types.BlockType.Text)
+                    {
+                        foreach (var para in block.Paragraphs)
+                        {
+                            foreach (var word in para.Words)
+                            {
+                                foreach (var sym in word.Symbols)
+                                {
+                                    Console.Write(sym.Text);
+                                }
+                            }
+                        }
+                    }
+                    else if (block.BlockType == Block.Types.BlockType.Table)
+                    {
+                        Console.WriteLine("found a table. don't know how to handle this yet!");
+                    }
+                }
+            }
             Console.WriteLine(response.Text);
             Console.ReadLine();
         }
